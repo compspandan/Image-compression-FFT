a14156672fd53bac50fa240dee15e7a1dfc46f0c
ubuntu <compspandan@gmail.com>
2021-11-19 22:35:48 +0530

    fixed the pv mul function

--- solution.py
+++ solution.py
@@ -11,18 +11,24 @@ def test_dft_fft_1n2():
     print(np.allclose(d, np.fft.fft(v1.cv)))
 
 
-def pv_mul(fft1, fft2):
-    l1 = len(fft1)
-    l2 = len(fft2)
-    l3 = l1 + l2
+def pv_mul(fft1,fft2,poly1,poly2):
+	l1 = len(fft1)
+	l2 = len(fft2)
+	l3 = l1 + l2
+	for _ in range(l3-l1):
+		fft1 = np.append(fft1,poly1.cv[0])
+	for _ in range(l3-l2):
+		fft2 = np.append(fft2,poly2.cv[0])
+	return fft1*fft2
 
 
 def test_pv_mul_3():
-    poly1 = polynomial()
-    poly2 = polynomial()
-    fft1 = poly1.fft()
-    fft2 = poly2.fft()
-    pv_mul(fft1, fft2)
+	poly1 = polynomial()
+	poly2 = polynomial()
+	fft1 = poly1.fft()
+	fft2 = poly2.fft()
+	pv = pv_mul(fft1,fft2,poly1,poly2)
+	print(pv)
 
 
 def convolution_check():
